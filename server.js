cbece09149be8fe4cb7e9e4f202a14ae006f0cfa
Christian Jones <c529j466@yahoo.com>
2022-08-29 04:35:32 -0500

    added notes route

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
