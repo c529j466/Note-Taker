203de3f89f13abc7fee39a1ffa8332bab44f9739
Christian Jones <c529j466@yahoo.com>
2022-08-30 19:10:44 -0500

    added post route for notes and added persistance to new notes based on assigned ids

--- server.js
+++ server.js
@@ -30,12 +30,13 @@ app.get('/notes', (req, res) =>
 // Returns callbacks as promises 
 const readContent = util.promisify(fs.readFile);
 
-
+// Creates function for writing content to the db.json
 const writeContent = (location, content) => 
     fs.writeFile(location, JSON.stringify(content, null, 4), (err) => 
         err ? console.error(err) : console.info(`\nData written to ${location}`)
     );
 
+// Creates function for appending db.json 
 const appendContent = (content, file) => {
     fs.readFile(file, "utf8", (err, data) => {
         if (err) {
@@ -48,11 +49,35 @@ const appendContent = (content, file) => {
     });
 };
 
+// Attaching readContent function to notes for persistance
 app.get("/api/notes", (req, res) => {
     console.info(`${req.method} request received to get notes`)
     readContent("./db/db.json").then((data) => res.status(200).json(JSON.parse(data)));
 });
 
+// Creates POST route for notes
+app.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    console.info(req.rawHeaders);
+
+// Sets title and text to the body of the request
+    const { title, text } = req.body;
+// Sets an id to the request using UUID function from helpers folder
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+// Sets new note to the db.json file 
+        appendContent(newNote, "./db/db.json");
+
+        return res.status(201).json(response);
+    } 
+    else {
+        return res.status(500).json("Error in posting note");
+    }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
